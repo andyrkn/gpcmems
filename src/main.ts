bc94d4463743c7e97f2391a9e9fdbf88a927212f
Mihaila Andrei <andyfbgm@gmail.com>
2019-03-02 22:13:27 +0200

    xd

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ function start(): void {
     const renderer: WebGLRenderer = new WebGLRenderer(canvas.getContext("webgl"));
 
     let counter = 0;
-    const functions: Function[] = [f1, f2, f3, f4, f5, f6, f7, f8];
-
+    const functions: Function[] = [f1, f7];
+    functions[counter](renderer);
     document.addEventListener("keydown", (e) => {
         if (e.key == "ArrowRight") {
             if (counter == functions.length - 1) counter = 0;
@@ -35,93 +35,6 @@ function f1(renderer: WebGLRenderer) {
 
     renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "LINE_STRIP");
 }
-
-function f2(renderer: WebGLRenderer) {
-    let coordinates = [];
-    let colors = [];
-    const a = 0.3;
-    const b = 0.2;
-
-
-    for (let t = -Math.PI; t <= Math.PI; t = t + 0.05) {
-        const x = 2 * (a * Math.cos(t) + b) * Math.cos(t) / 1.2;
-        const y = 2 * (a * Math.cos(t) + b) * Math.sin(t);
-
-        coordinates.push(x, y, 0);
-        colors.push(1, 0, 0);
-    }
-
-    renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "LINE_LOOP");
-}
-
-function f3(renderer: WebGLRenderer) {
-    let coordinates = [];
-    let colors = [];
-    const a = 0.2;
-
-    coordinates.push(-1, 1, 0);
-    colors.push(1, 0, 0);
-    for (let t = -Math.PI / 2; t <= Math.PI / 2; t = t + 0.05) {
-
-        if (Math.abs(t) != Math.PI / 6) {
-            const x = a / (4 * Math.cos(t) * Math.cos(t) - 3) / 5;
-            const y = (a * Math.tan(t)) / (4 * Math.cos(t) * Math.cos(t) - 3) / 5;
-
-            coordinates.push(x, y, 0);
-            colors.push(1, 0, 0);
-        }
-    }
-    renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "TRIANGLE_FAN");
-}
-
-function f4(renderer: WebGLRenderer) {
-    let coordinates = [];
-    let colors = [];
-    const a = 0.1;
-    const b = 0.2;
-
-    for (let t = -9.5; t <= 9.5; t = t + 0.05) {
-        const x = a * t - b * Math.sin(t);
-        const y = a - b * Math.cos(t);
-
-        coordinates.push(x, y, 0);
-        colors.push(1, 0, 0);
-    }
-    renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "LINE_STRIP");
-}
-
-function f5(renderer: WebGLRenderer) {
-    let coordinates = [];
-    let colors = [];
-    const a = 0.1;
-    const b = 0.3;
-
-    for (let t = 0; t <= Math.PI * 2; t = t + 0.05) {
-        const x = (a + b) * Math.cos(b / a * t) - b * Math.cos(t + b / a * t);
-        const y = (a + b) * Math.sin(b / a * t) - b * Math.sin(t + b / a * t);
-
-        coordinates.push(x, y, 0);
-        colors.push(1, 0, 0);
-    }
-    renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "LINE_STRIP");
-}
-
-function f6(renderer: WebGLRenderer) {
-    let coordinates = [];
-    let colors = [];
-    const a = 0.1;
-    const b = 0.3;
-
-    for (let t = 0; t <= Math.PI * 2; t = t + 0.05) {
-        const x = (a - b) * Math.cos(b / a * t) - b * Math.cos(t - b / a * t);
-        const y = (a - b) * Math.sin(b / a * t) - b * Math.sin(t - b / a * t);
-
-        coordinates.push(x, y, 0);
-        colors.push(1, 0, 0);
-    }
-    renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "LINE_STRIP");
-}
-
 function f7(renderer: WebGLRenderer) {
     let coordinates = [];
     let colors = [];
@@ -152,23 +65,4 @@ function f7(renderer: WebGLRenderer) {
         colors.push(1, 0, 0);
     }
     renderer.renderOver(new Float32Array(coordinates), new Float32Array(colors), "LINE_STRIP");
-}
-
-function f8(renderer: WebGLRenderer) {
-    let coordinates = [];
-    let colors = [];
-    const a = 0.02;
-
-    for (let t = 0; t <= 10; t = t + 0.05) {
-
-        const r = a * Math.pow(Math.E, 1 + t);
-
-        const x = r * Math.cos(t);
-        const y = r * Math.sin(t);
-
-        coordinates.push(x, y, 0);
-        colors.push(1, 0, 0);
-    }
-
-    renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "LINE_STRIP");
 }
