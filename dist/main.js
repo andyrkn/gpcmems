4e7a5822b118e3e8accbcddb39571675896bbc5b
Mihaila Andrei <andyfbgm@gmail.com>
2019-03-07 17:38:12 +0200

    del

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-function start() {
-    const canvas = document.getElementById("canvas");
-    const renderer = new WebGLRenderer(canvas.getContext("webgl"));
-    const tema = new Tema2(renderer);
-    tema.start();
-}
-start();
-//# sourceMappingURL=main.js.map
