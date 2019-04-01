3ff8afcbae7a5241842a39019f13952b4bd1b2c2
Mihaila Andrei <andyfbgm@gmail.com>
2019-04-01 17:14:21 +0300

    T3

--- src/ImplementedHomework.ts
+++ src/ImplementedHomework.ts
@@ -1,125 +1,141 @@
 class ImplementedHomework extends Homework {
-
-    public static nivel: number;
-
     constructor(renderer: WebGLRenderer) {
         super(renderer);
-        ImplementedHomework.nivel = 0;
-        document.addEventListener("keydown", (e) => {
-            if (e.key == "ArrowUp") {
-                ImplementedHomework.nivel++;
-                this.start();
-            }
-            if (e.key == "ArrowDown") {
-                ImplementedHomework.nivel > 0 ? ImplementedHomework.nivel-- : ImplementedHomework.nivel = 0;
-                this.start();
-            }
-
-        })
-        this.addFunctions([this.img1, this.img2, this.MandelBrot, this.img3]);
-        //this.addFunctions([this.img1, this.MandelBrot, this.JuliaFatou]);
+        this.addFunctions([this.f1]);
     }
 
-    private static juliaConverge(x: Complex, c: Complex): number {
+    private f1(renderer: WebGLRenderer) {
+        const display: CartezianDisplay = new CartezianDisplay(15);
+        display.drawGrid((coord: Float32Array, color: Float32Array) => { renderer.clearAndRender(coord, color, "LINES") });
 
-        let z1: Complex = Complex.set(x);
-        let z2: Complex = new Complex(0, 0);
+        const lo = new gLcoords(0, 15);
+        const ln = new gLcoords(15, 10);
+        display.drawLine(lo.x, lo.y, ln.x, ln.y, (coord: Float32Array, color: Float32Array) => { renderer.renderOver(coord, color, "TRIANGLE_FAN") });
 
-        for (let i = 0; i < 29; i++) {
-            z2 = z1.square().sum(c);
+        const l1 = display.toGlCoords(lo.x, lo.y);
+        const l2 = display.toGlCoords(ln.x, ln.y);
+        renderer.renderOver(new Float32Array([l1.x, l1.y, 0, l2.x, l2.y, 0]), new Float32Array([0, 0.5, 0, 0, 0.5, 0]), "LINES");
+        
+        const lo1 = new gLcoords(15, 8);
+        const ln1 = new gLcoords(7, 3);
+        display.drawLine(lo1.x, lo1.y, ln1.x, ln1.y, (coord: Float32Array, color: Float32Array) => { renderer.renderOver(coord, color, "TRIANGLE_FAN") });
 
-            if (z2.complexEquals(z1)) return 0;
-            if (z2.modul() > 10000) return 1;
-            z1 = z2;
-        }
-        return 2;
+        const l11 = display.toGlCoords(lo1.x, lo1.y);
+        const l21 = display.toGlCoords(ln1.x, ln1.y);
+        renderer.renderOver(new Float32Array([l11.x, l11.y, 0, l21.x, l21.y, 0]), new Float32Array([0, 0.5, 0, 0, 0.5, 0]), "LINES");
+    }
+}
+
+class CartezianDisplay {
+    public size: number;
+    constructor(size: number) {
+        this.size = size;
     }
 
-    private static mandelbrotConverge(x: Complex): number {
-
-        let z1: Complex = new Complex(0, 0);
-        let z2: Complex = new Complex(0, 0);
-
-        for (let i = 0; i < 1000; i++) {
-            z2 = z1.square().sum(x);
-
-            if (z2.modul() > 2) {
-                return i;
-            }
-            z1 = z2;
-        }
-        return -1;
+    public toGlCoords(x: number, y: number): gLcoords {
+        return new gLcoords(x / this.size * 2 - 1, y / this.size * 2 - 1);
     }
 
-    private JuliaFatou(renderer: WebGLRenderer): void {
-        let coordinates = [];
+    public drawGrid(callback: Function): void {
         let colors = [];
+        let coords = [];
+        for (let i = 0; i <= this.size; i++) {
+            const horiz = this.toGlCoords(-1, i);
+            coords.push(-1, horiz.y, 0, 1, horiz.y, 0);
+            colors.push(0, 0, 0, 0, 0, 0);
 
-        const ratio = 0.001;
-        const limits = [-1, 1, -1, 1];
-        const conq: Complex = new Complex(-0.012, 0.74);
-
-        for (let x = limits[0]; x < limits[1]; x = x + ratio) {
-            for (let y = limits[2]; y < limits[3]; y = y + ratio) {
-                const c: Complex = new Complex(x, y);
-                let conv: number = ImplementedHomework.juliaConverge(c, conq);
-
-                if (conv == 2) {
-                    coordinates.push(x, y, 0);
-                    colors.push(1, 0.5, 0);
-                }
-                else if (conv == 1) {
-                    coordinates.push(x, y, 0);
-                    colors.push(0.0, 0, 0.0);
-                } else {
-                    coordinates.push(x, y, 0);
-                    colors.push(1, 1, 1);
-                }
-            }
+            const vertic = this.toGlCoords(i, 1);
+            coords.push(vertic.x, 1, 0, vertic.x, -1, 0);
+            colors.push(0, 0, 0, 0, 0, 0);
         }
-        renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "POINTS");
+        callback(new Float32Array(coords), new Float32Array(colors));
     }
 
-    private MandelBrot(renderer: WebGLRenderer): void {
-        let coordinates = [];
+    public drawLine(x0: number, y0: number, xn: number, yn: number, callback: Function): void {
+        if (x0 <= xn && y0 <= yn) { this.drawNE(x0, y0, xn, yn, callback); }
+        else if (x0 <= xn && y0 > yn) { this.drawSE(x0, y0, xn, yn, callback); }
+        else if (x0 >= xn && y0 >= yn) { this.drawNE(xn, yn, x0, y0, callback); }
+        else this.drawSE(xn, yn, x0, y0, callback);
+    }
+
+    private drawNE(x0: number, y0: number, xn: number, yn: number, callback: Function): void {
+        this.markPoint(x0, y0, callback);
+        const dx = xn - x0;
+        const dy = yn - y0;
+        let d, dE, dNE;
+
+        if (dx < dy) {
+            d = 2 * dx - dy;
+            dE = 2 * dx;
+            dNE = 2 * (dx - dy);
+
+            while (y0 < yn) {
+                if (d < 0) { d += dE; y0++; }
+                else { d += dNE; x0++; y0++; }
+                this.markPoint(x0, y0, callback);
+            }
+        } else {
+            d = 2 * dy - dx;
+            dE = 2 * dy;
+            dNE = 2 * (dy - dx);
+
+            while (x0 < xn) {
+                if (d < 0) { d += dE; x0++; }
+                else { d += dNE; x0++; y0++; }
+                this.markPoint(x0, y0, callback);
+            }
+        }
+    }
+
+    private drawSE(x0: number, y0: number, xn: number, yn: number, callback: Function): void {
+        this.markPoint(x0, y0, callback);
+        const dx = xn - x0;
+        const dy = Math.abs(yn - y0);
+        let d, dE, dSE;
+
+        if (dx < dy) {
+            d = 2 * dx - dy;
+            dE = 2 * dx;
+            dSE = 2 * (dx - dy);
+
+            while (y0 > yn) {
+                if (d < 0) { d += dE; y0--; }
+                else { d += dSE; x0++; y0--; }
+                this.markPoint(x0, y0, callback);
+            }
+        } else {
+            d = 2 * dy - dx;
+            dE = 2 * dy;
+            dSE = 2 * (dy - dx);
+
+            while (x0 < xn) {
+                if (d < 0) { d += dE; x0++; }
+                else { d += dSE; x0++; y0--; }
+                this.markPoint(x0, y0, callback);
+            }
+        }
+    }
+
+    private markPoint(x: number, y: number, callback: Function): void {
+        let coords = [];
         let colors = [];
-
-        const ratio = 0.005;
-        const limits = [-2.0, 2.0, -2.0, 2.0];
-
-        for (let x = limits[0]; x < limits[1]; x = x + ratio) {
-            for (let y = limits[2]; y < limits[3]; y = y + ratio) {
-                const c: Complex = new Complex(x, y);
-                const conv: number = ImplementedHomework.mandelbrotConverge(c);
-
-                if (conv == -1) {
-                    coordinates.push(x / 2, y / 2, 0);
-                    colors.push(1, 0.5, 0);
-                }
-                else {
-                    coordinates.push(x / 2, y / 2, 0);
-                    colors.push(0.5, conv / 1000, 1);
-                }
-            }
+        let step = 0.1;
+        let radius = 0.012;
+        const point = this.toGlCoords(x, y);
+        for (let d = 0; d <= 2 * Math.PI - step; d = d + step) {
+            coords.push(Math.sin(d) * radius + point.x, Math.cos(d) * radius + point.y, 0);
+            colors.push(1, 0, 0);
         }
-        renderer.clearAndRender(new Float32Array(coordinates), new Float32Array(colors), "POINTS");
+        callback(new Float32Array(coords), new Float32Array(colors));
     }
 
-    private img1(renderer: WebGLRenderer): void {
-        renderer.clearAndRender(new Float32Array([]), new Float32Array([]), "POINTS");
-        const square = new Square(ImplementedHomework.nivel);
-        square.draw(0, 0, 1, 0.5, (coord: Float32Array, cl: Float32Array) => { renderer.renderOver(coord, cl, "LINES"); });
-    }
+}
 
-    private img2(renderer: WebGLRenderer): void {
-        const ftree = new FractalTree(ImplementedHomework.nivel);
-        renderer.clearAndRender(new Float32Array([-0.3, 1, 0, -0.3, 0.8, 0]), new Float32Array([1, 0, 0, 1, 0, 0]), "LINES");
-        ftree.draw(-0.3, 0.8, 1, 0.3, (coord: Float32Array, cl: Float32Array) => { renderer.renderOver(coord, cl, "LINES"); }, new Vector(0, -1));
-    }
-
-    private img3(renderer: WebGLRenderer): void {
-        renderer.clearAndRender(new Float32Array([]), new Float32Array([]), "POINTS");
-        const trengle = new WeirdTriangle(ImplementedHomework.nivel);
-        trengle.draw(0, 0, 1, 0.1, (coord: Float32Array, cl: Float32Array) => { renderer.renderOver(coord, cl, "LINES"); }, new Vector(0, 1), 1);
+class gLcoords {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
     }
 }
